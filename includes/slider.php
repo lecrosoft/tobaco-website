d0c5f6eb4993487597aacf804b509fefa90cd68f
lecrosoft <lecrosoft@gmail.com>
2022-07-09 00:13:10 +0100

    new update

--- includes/slider.php
+++ includes/slider.php
@@ -19,7 +19,7 @@
                  <img class="img-fluid" src="img/Corona-especial.png" alt="" srcset="" width="100%">
                  <h5 class="text-center headings py-3">Half Corona</h5>
                  <p class="text-center light_gold_heading" style="margin-top:-1rem">
-                     4 * 4 Tobaco super
+                     4 * 4 Tobaco super Dopper
                  </p>
              </div>
              <div class="testimonial-item bg-transparent border rounded">
